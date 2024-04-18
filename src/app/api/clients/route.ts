974d87c73b6b279d03201e6d62b6f712f056fea5
--global <--global>
2024-04-18 09:07:03 -0300

    Com Thunder

---
+++ src/app/api/clients/route.ts
@@ -0,0 +1,33 @@
+import { Client } from '@/app/api/clients/interface'
+
+const clients: Client[] = [
+    {
+        name: 'Lucas Minozzo',
+        address: 'Rua da rua'
+    },
+    {
+        name: 'Marcelo Galli',
+        address: 'Rua da rua'
+    },
+    {
+        name: 'Diego Costa',
+        address: 'Porto Alegre'
+    }
+];
+
+export function GET(){
+    return Response.json(clients);
+}
+
+export async function POST(request: Request) {
+    const form = await request.formData();
+    const name = form.get('name') as string;
+    const address = form.get('address') as string;
+
+    clients.push({
+        name,
+        address,
+    });
+
+    return Response.json({status: "Added"});
+}
